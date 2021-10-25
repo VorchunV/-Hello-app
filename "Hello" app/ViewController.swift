81a4c4168d17cd210977f9617294fd5271289b72
VorchunV <varenkoluchka@gmail.com>
2021-10-25 22:46:15 +0300

    Initial Commit

---
+++ "Hello" app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  "Hello" app
+//
+//  Created by Варвара Фомина on 25.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
